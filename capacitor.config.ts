9677cc2ab0c3dd9b2a01e70f50a2014b16c7a7d5
tobiolayiwola <tobiolayiwola1@gmail.com>
2025-10-02 14:31:46 -0500

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tryeidolon.app',
+  appName: 'Eidolon',
+  webDir: 'dist'
+};
+
+export default config;
